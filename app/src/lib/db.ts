923ba5b434d2ca9e3ad4be4236e7aaf24564318e
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-02-09 12:53:45 +0100

    Add mysql libs

--- app/src/lib/db.ts
+++ app/src/lib/db.ts
@@ -17,3 +17,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 export const db = prisma;
+
+import mysql from 'serverless-mysql';
+
+// const dba = mysql({
+//   config: {
+//     // host: process.env.MYSQL_HOST,
+//     // port: parseInt(process.env.MYSQL_PORT || '3306'),
+//     // database: process.env.MYSQL_DATABASE,
+//     // user: process.env.MYSQL_USER,
+//     // password: process.env.MYSQL_PASSWORD
+//   }
+// });
