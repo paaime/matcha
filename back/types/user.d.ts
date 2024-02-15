5779829c3bd9cc1d31592a9f2fd2f9eb124ade4a
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-02-15 22:03:51 +0100

    Add methods

--- back/types/user.d.ts
+++ back/types/user.d.ts
@@ -10,15 +10,16 @@ export interface IUser {
   age: number;
   gender: string;
   sexualPreferences: string;
-  distance?: number;
-  biography?: string;
+	distance?: number; // Can be undefined
+	city?: string; // Can be undefined
+  biography?: string; // Can be undefined
 	interests: ITag[];
-	pictures?: string;
+	pictures?: string; // Can be undefined
   fameRating: number;
 	isMatch: boolean;
 	isLiked: boolean;
 	hasLiked: boolean;
 	isBlocked: boolean;
 	hasBlocked: boolean;
-	matchId?: string; // If isMatch = true
+	matchId?: string; // If isMatch = true ; can be undefined
 }
