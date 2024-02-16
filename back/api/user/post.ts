0f563fde3ef03b7a06f4c48cbc8c04ca2d5c86ba
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-02-16 19:43:28 +0100

    Clear 'token' cookie on addUser and loginUser

--- back/api/user/post.ts
+++ back/api/user/post.ts
@@ -8,10 +8,16 @@ import { loginUser } from './functions/loginUser';
 const router = express.Router();
 
 router.post('/', async (req: Request, res: Response) => {
+  // Clear cookie
+  res.clearCookie('token');
+  
   await addUser(req.body, res);
 });
 
 router.post('/login', async (req: Request, res: Response) => {
+  // Clear cookie
+  res.clearCookie('token');
+
   await loginUser(req.body, res);
 });
 
