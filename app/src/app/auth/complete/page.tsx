9cdf52968363c0581d5ad2a9140ae328b6f1c244
Paulooos <pauloaimp@gmail.com>
2024-02-23 16:43:50 +0100

    Add animation and page title

--- app/src/app/auth/complete/page.tsx
+++ app/src/app/auth/complete/page.tsx
@@ -1,5 +1,9 @@
 import CompleteForm from '@/components/Complete/Form';
 
+export const metadata = {
+  title: 'Matcha | Complete your profile',
+};
+
 export default async function Complete() {
   return (
     <div className="mx-auto flex w-full flex-col space-y-6 h-full justify-between">
