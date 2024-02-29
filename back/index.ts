c250b38b862c75701cf75165c9a09202475c3ed7
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-02-29 17:38:18 +0100

    Temp remove generation of random users

--- back/index.ts
+++ back/index.ts
@@ -59,7 +59,7 @@ initializeIO(server);
 
 // Start web server
 server.listen(PORT, async () => {
-  await randUser(10);
+  // await randUser(10); // ! Uncomment to generate random users
 
   console.log(
     `API and WebSocket server is running at http://localhost:${PORT}`
