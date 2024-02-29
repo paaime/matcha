72e27b4410b480a7aedb3e483dfdd6e6df7c13d5
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-02-29 21:20:30 +0100

    Update infos on discovery page

--- app/src/components/Discover/Header.tsx
+++ app/src/components/Discover/Header.tsx
@@ -1,15 +1,34 @@
 'use client';
 
-import { MapPinIcon, SearchIcon } from 'lucide-react';
+import { MapPinIcon } from 'lucide-react';
 import Search from './Search';
-import { Button } from '../ui/button';
 import Filters from './Filters';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useUserStore } from '@/store';
+import customAxios from '@/utils/axios';
+import { toast } from 'sonner';
 
 export default function Header() {
   const { user } = useUserStore();
   const [searchOpen, setSearchOpen] = useState(false);
+
+  const setUser = useUserStore((state) => state.setUser);
+
+  // Re call the user to get the updated list of blocked, visited and history
+  const getUser = async () => {
+    try {
+      const { data } = await customAxios.get('/user/me');
+      setUser(data);
+    } catch (err) {
+      if (err.response?.data?.message) toast.error(err.response.data.message);
+      else toast.error('An error occured');
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="flex justify-between">
       {searchOpen ? (
