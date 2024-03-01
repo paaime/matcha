79344a17717bcf43fefe66780df84264fd79ac25
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-03-01 12:12:33 +0100

    Add my marker on profile page

--- app/src/components/Discover/Header.tsx
+++ app/src/components/Discover/Header.tsx
@@ -1,33 +1,29 @@
 'use client';
 
 import { MapPinIcon } from 'lucide-react';
-import Search from './Search';
 import Filters from './Filters';
-import { useState } from 'react';
 import { useUserStore } from '@/store';
 
 export default function Header() {
   const { user } = useUserStore();
-  const [searchOpen, setSearchOpen] = useState(false);
+
+  const city = user?.consentLocation ? user?.city : 'Everywhere';
 
   return (
     <div className="flex justify-between">
-      {searchOpen ? (
-        <Search />
-      ) : (
-        <div className="flex flex-col gap-1">
-          <div className="flex gap-2">
-            <MapPinIcon className="h-4 w-4 stroke-pink" />
-            <span className="text-sm font-semibold">
-              {user?.city || 'Everywhere'}
-            </span>
-          </div>
-          <h2 className="text-3xl font-extrabold text-black dark:text-white">
-            Discover
-          </h2>
-          <p className="text-gray-400">Our last members</p>
+      <div className="flex flex-col gap-1">
+        <div className="flex gap-2">
+          <MapPinIcon className="h-4 w-4 stroke-pink" />
+          <span className="text-sm font-semibold">
+            {city || 'Everywhere'}
+          </span>
         </div>
-      )}
+        <h2 className="text-3xl font-extrabold text-black dark:text-white">
+          Discover
+        </h2>
+        <p className="text-gray-400">Our last members</p>
+      </div>
+
       <div className="flex gap-3 items-center">
         <Filters />
       </div>
