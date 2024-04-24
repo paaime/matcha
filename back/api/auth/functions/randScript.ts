db945d957716af26a8000d22f807eb8f77b5185d
Damien Vergobbi <dvergobb@z3r5p4.42lyon.fr>
2024-04-24 16:35:41 +0200

    fix minor bug

--- back/api/auth/functions/randScript.ts
+++ back/api/auth/functions/randScript.ts
@@ -124,15 +124,10 @@ const addRandom = async(db: Connection): Promise<boolean> => {
   }
 }
 
-// TODO restaure
-// const NB_USERS = 500;
-// const NB_LIKES = 15;
-// const NB_TAGS = 3;
-// const NB_VISITS = 7;
-const NB_USERS = 50;
-const NB_LIKES = 1;
+const NB_USERS = 500;
+const NB_LIKES = 15;
 const NB_TAGS = 3;
-const NB_VISITS = 1;
+const NB_VISITS = 7;
 
 export async function randScript(): Promise<boolean> {
 
